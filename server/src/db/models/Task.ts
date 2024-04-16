17108f5d43d572aa922718b716ac1bb4dc9c2285
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-04-16 19:08:39 +0200

    Update Task model to use IUser interface for user field

--- server/src/db/models/Task.ts
+++ server/src/db/models/Task.ts
@@ -1,20 +1,20 @@
 import mongoose, { Schema, Document } from 'mongoose';
-import User  from './user';
+import { IUser } from './user'; // Import IUser interface from user.ts
 
 export interface ITask extends Document {
   name: string;
   completed: boolean;
   createdAt: Date;
   updatedAt: Date;
-  user: User['_id']
+  user: IUser['_id']; 
 }
 
 const TaskSchema: Schema = new Schema({
   name: { type: String ,required: true},
   completed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  updatedAt: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: 'User'}
 });
-
 
 export default mongoose.model<ITask>('Task', TaskSchema);
