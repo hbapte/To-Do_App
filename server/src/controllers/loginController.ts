bdb7e3de8153bdb277dbecf2d06080c7f47d3031
ISHIMWE Jean Baptiste <ijbapte@gmail.com>
2024-04-16 19:08:20 +0200

    Update JWT token expiration to 1 hour for rememberMe option in loginController.ts

--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -26,7 +26,7 @@ const loginController = async (req: Request, res: Response) => {
     }
 
      // JWT token with JWT_SECRET
-     const expiresIn = rememberMe ? '7d' : '1m'; 
+     const expiresIn = rememberMe ? '7d' : '1h'; 
      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn });
  
      console.log('Login successful');
@@ -40,51 +40,3 @@ const loginController = async (req: Request, res: Response) => {
 };
 
 export default loginController;
-
-// import { Router, Request, Response } from 'express';
-// import User from '../db/models/user';
-// import { validateLogin } from '../middlewares/loginValidationMiddleware';
-// const bcrypt = require('bcrypt'); 
-// const jwt = require('jsonwebtoken');
-
-
-// const router = Router();
-
-
-// router.post('/', validateLogin, async (req: Request, res: Response) => {
-//   const { 'email-username': emailOrUsername, 'password-': password, 'remember-me': rememberMe  } = req.body;
-
-//   try {
-//     // Check if the user exists by username or email
-//     const user = await User.findOne({
-//       $or: [{ username: emailOrUsername }, { email: emailOrUsername }]
-//     });
-//     if (!user) {
-//       console.log('User not found');
-//       return res.status(404).json({ error: 'User not found' });
-//     }
-
-//     // console.log('Retrieved User:', user);
-
-//     // Compare the passwords using bcrypt
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
-//     if (!isPasswordValid) {
-//       console.log('Invalid password');
-//       return res.status(401).json({ error: 'Wrong Password' });
-//     }
-
-//     // JWT token with JWT_SECRET
-//     const expiresIn = rememberMe ? '7d' : '1h'; 
-//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn });
-
-//     console.log('Login successful');
-//     res.cookie('token', token, { httpOnly: true }); 
-//     res.redirect('/dashboard');
-//   } catch (error) {
-//     console.error('Error logging in user:', error);
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-
-// export { router as loginController };
